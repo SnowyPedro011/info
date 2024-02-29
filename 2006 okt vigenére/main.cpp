d19813c954ad1ffa366f34c5d0341ebd4d8e036c
SnowyPedro011 <159954865+SnowyPedro011@users.noreply.github.com>
2024-02-29 23:02:39 +0100

    Add files via upload

---
+++ 2006 okt vigenére/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+int main()
+{
+    // 1. feladat
+
+    string x , (szovegsor[255] [255])={};
+    cout << "kerek maximum 255 karakternyi nem ures szoveget";
+    cin >> szovegsor;
+
+
+    //2. feladat
+
+    /// megváltoztatni a bevett szöveget a 'o' re és 'a' ra emelett 'o' --> 'O'
+
+
+    //3. feladat
+
+    cout << szovegsor; ///atalakitott
+
+
+    //4. feladat
+
+    cout << "kerek egy max 5 karakteres nem ures kulcsszot";
+    cin >> x ;
+    /// 'o' -->'O'
+
+
+    //5. feladat
+
+    /// az x kulcsszot megsokszorozni mivel 255 ben megvan az 5 pontosan pontosan is beszorozhatjuk ez a 51x-e a kulcsszonak
+    /// tehát x ctrl+C ctrl+V 51 szer
+
+
+    //6. feladat
+
+    string karakter;
+    ///szovegsor[1] -> keresés tétele
+    ///kulcsszoveg[n][1] -> kereses csak az oszlopban valahogy léptetni maybe
+
+    /*
+    int n = 0;
+    int i = 0;
+
+while( [n]i < 255 && !(karakter==tabla) )
+{
+    i++;
+}
+
+if( [n]i < 255 )
+{
+   int oszam=i;
+}
+
+*/
+
+
+
+
+/*
+    int i = 0;
+
+while( i < 255 && !(karakter==tabla) )
+{
+    i++;
+}
+
+if( i < 255 )
+{
+   int sszam=i;
+}
+
+*/
+
+
+
+/// belerakjuk a két i-t amit kaptunk egy [255] [255] elemube és az indexuk alapján vissza adunk egy betût
+
+
+    return 0;
+}
